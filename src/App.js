cd697e5f9b8985ef21bb72d7ef0a1e703f3e44c8
saikiran-seepana <saikiranseepana@gmail.com>
2025-04-09 03:08:02 +0530

    Added footer and merged main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect }  from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
@@ -7,34 +7,44 @@ import ProfessionalTimeline from "./components/ProfessionalTimeline";
 import MouseClickEffect from "./components/MouseClickEffect";
 
 import "./App.css";
+import Footer from "./components/Footer";
 
 const App = () => {
   useEffect(() => {
     const handleScroll = () => {
       const navbar = document.getElementById("navbar");
-      const fullscreenDivHeight = document.querySelector(".about-description").offsetHeight;
+      const fullscreenDivHeight =
+        document.querySelector(".about-description").offsetHeight;
 
       if (window.scrollY > fullscreenDivHeight) {
-        navbar.style.display = "flex";  // Show the navbar when scrolled beyond the fullscreen div
+        navbar.style.display = "flex"; // Show the navbar when scrolled beyond the fullscreen div
       } else {
-        navbar.style.display = "none";  // Hide the navbar when within the fullscreen div
+        navbar.style.display = "none"; // Hide the navbar when within the fullscreen div
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll); // Cleanup event listener on unmount
+      window.removeEventListener("scroll", handleScroll); // Cleanup event listener on unmount
     };
   }, []);
   return (
     <Router>
       <MouseClickEffect />
-      <section id="about"><About /></section>
+      <section id="about">
+        <About />
+      </section>
       <Navbar />
-      <section id="experience"><ProfessionalTimeline /></section>
-      <section id="contact"><Contact /></section>
+      <section id="experience">
+        <ProfessionalTimeline />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
 
+      <ProfessionalTimeline />
+      <Footer />
     </Router>
   );
 };
